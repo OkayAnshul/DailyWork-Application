d140e03b1549270fecbec986d630021defd6a9c2
Anshul Kumar <anshulisokay@gmail.com>
2024-12-07 05:28:14 +0530

    first commit

---
+++ app/src/main/java/com/example/dailywork/uiElements/EditScreen.kt
@@ -0,0 +1,7 @@
+package com.example.dailywork.uiElements
+
+import androidx.compose.ui.Modifier
+
+fun EditScreen(modifier: Modifier){
+
+}
---
+++ app/src/main/java/com/example/dailywork/uiElements/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.dailywork.uiElements
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(modifier: Modifier){
+    Column {  }
+
+}
