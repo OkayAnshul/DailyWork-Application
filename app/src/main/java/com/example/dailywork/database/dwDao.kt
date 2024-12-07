b4717e7790fd80883ea27b608a6b78693db42570
Anshul Kumar <anshulisokay@gmail.com>
2024-12-07 06:03:40 +0530

    Dao interface setup for DB

---
+++ app/src/main/java/com/example/dailywork/database/dwDao.kt
@@ -0,0 +1,22 @@
+package com.example.dailywork.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface dwDao {
+    @Query("Select * from dw_table order by id asc")
+    suspend fun getAllTask():Flow<List<Task>>
+    @Insert
+    suspend fun createTask(task:Task)
+    @Update
+    suspend fun updateTask(task: Task)
+    @Delete
+    suspend fun deleteTask(task: Task)
+    @Query("select * from dw_table where id=:id ")
+    suspend fun getTask(id:Long):Flow<Task>
+}
