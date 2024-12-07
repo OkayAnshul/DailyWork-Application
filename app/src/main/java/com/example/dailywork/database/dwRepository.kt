fc16bf419269a61564e9579a0e3f439bda3dcf9e
Anshul Kumar <anshulisokay@gmail.com>
2024-12-07 06:59:29 +0530

    Repository is created for external calling of dao functions

---
+++ app/src/main/java/com/example/dailywork/database/dwRepository.kt
@@ -0,0 +1,21 @@
+package com.example.dailywork.database
+
+import kotlinx.coroutines.flow.Flow
+
+class dwRepository(private val dao:dwDao) {
+
+    val getAllTask:Flow<List<Task>> = dao.getAllTask()
+
+    suspend fun getTask(id:Long){
+        dao.getTask(id)
+    }
+    suspend fun createTask(task: Task){
+        dao.createTask(task)
+    }
+    suspend fun deleteTask(task: Task){
+        dao.deleteTask(task)
+    }
+    suspend fun updateTask(task: Task){
+        dao.updateTask()
+    }
+}
