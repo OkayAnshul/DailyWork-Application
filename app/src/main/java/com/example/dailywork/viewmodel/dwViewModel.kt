8965e9ad40caccc3434800ed207d3f1096a24ce0
Anshul Kumar <anshulisokay@gmail.com>
2024-12-07 13:06:31 +0530

    dwViewModel created with repository injection

---
+++ app/src/main/java/com/example/dailywork/viewmodel/dwViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.dailywork.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dailywork.database.Task
+import com.example.dailywork.database.dwRepository
+import kotlinx.coroutines.launch
+
+class dwViewModel(private val repository: dwRepository) :ViewModel(){
+
+    var allTask by mutableStateOf<List<Task>>(emptyList())
+
+    init {
+        viewModelScope.launch {
+            repository.getAllTask.collect{
+                task ->
+                allTask = task
+            }
+        }
+    }
+    /*
+     //StateFlow to expose the task list to the UI
+    val allTasks: StateFlow<List<Task>> = repository.allTasks
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Lazily,
+            initialValue = emptyList()
+        )
+        */
+
+    //This mutableState flow works best with compose ui(jetpack compose)
+//But alternative which is using stateIn is also very good
+    fun addTask(task:Task){
+        viewModelScope.launch {
+            repository.createTask(task)
+        }
+    }
+    fun removeTask(task:Task){
+        viewModelScope.launch {
+            repository.deleteTask(task)
+        }
+    }
+    fun updateTask(task:Task){
+        viewModelScope.launch {
+            repository.updateTask(task)
+        }
+    }
+    fun getTask(id:Long){
+        viewModelScope.launch {
+            repository.getTask(id)
+        }
+    }
+}
