f3a5fd800c961acbc76168b66c952bd8ffaab224
Anshul Kumar <anshulisokay@gmail.com>
2024-12-07 05:53:17 +0530

    Task data class setup for Database using room

--- app/src/main/java/com/example/dailywork/database/Task.kt
+++ app/src/main/java/com/example/dailywork/database/Task.kt
@@ -1,2 +1,14 @@
 package com.example.dailywork.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalTime
+
+
+@Entity(tableName ="dw_table")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int=0,
+    val title:String,
+    val description:String,
+    val time:LocalTime)
