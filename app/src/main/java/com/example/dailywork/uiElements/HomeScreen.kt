d3c01616640a1f24d5f1e937824cfd020c6762eb
Anshul Kumar <anshulisokay@gmail.com>
2024-12-07 06:03:58 +0530

    modification in UI

--- app/src/main/java/com/example/dailywork/uiElements/HomeScreen.kt
+++ app/src/main/java/com/example/dailywork/uiElements/HomeScreen.kt
@@ -1,12 +1,20 @@
 package com.example.dailywork.uiElements
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(modifier: Modifier){
-    Column {  }
+    LazyColumn(modifier =modifier.padding(2.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+    }
 
 }
